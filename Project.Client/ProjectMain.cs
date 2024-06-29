ed1fc2375fdf3b26ea14825236d70c9eb02a00ce
local9 <apjcookie1@hotmail.com>
2024-06-29 17:47:42 +0100

    chore: initial resource setup

--- Project.Client/ProjectMain.cs
+++ Project.Client/ProjectMain.cs
@@ -1,7 +1,17 @@
-﻿namespace Project.Client
-{
-    public class ProjectMain
-    {
+﻿using AltV.Net.Client;
 
+namespace Project.Client
+{
+    internal class ProjectMain : Resource
+    {
+        public override void OnStart()
+        {
+            Console.WriteLine("Client Resource started");
+        }
+
+        public override void OnStop()
+        {
+            Console.WriteLine("Client Resource stopped");
+        }
     }
 }
--- Project.Server/ProjectMain.cs
+++ Project.Server/ProjectMain.cs
@@ -1,7 +1,17 @@
-﻿namespace Project.Server
-{
-    public class ProjectMain
-    {
+﻿using AltV.Net;
 
+namespace Project.Server
+{
+    internal class ProjectMain : Resource
+    {
+        public override void OnStart()
+        {
+            Console.WriteLine("Server Resource started");
+        }
+
+        public override void OnStop()
+        {
+            Console.WriteLine("Server Resource stopped");
+        }
     }
 }
