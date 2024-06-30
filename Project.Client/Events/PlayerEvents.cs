b962c979a11ce2d440f269fde2d1d99876be01ba
local9 <apjcookie1@hotmail.com>
2024-06-30 16:38:13 +0100

    chore: remove nuget package

---
+++ Project.Client/Events/PlayerEvents.cs
@@ -0,0 +1,13 @@
+﻿using AltV.Net;
+
+namespace Project.Client.Events
+{
+    internal class PlayerEvents : IScript
+    {
+        [ServerEvent("myEvent")]
+        public void MyEvent(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
